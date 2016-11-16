c65cf3c5a2cc71f275f6490ccbb35b67ac08a091
paul wright <pwright18@VenerableBede.schools.sunderland.gov.uk>
2016-11-16 11:51:20 +0000

    do it at home

--- adventure.py
+++ adventure.py
@@ -1,8 +1,42 @@
 # An adventure game
-
+from random import randint
+from time import sleep
 def room1():
+    print("Welcome To Soul Searcher")
+    sleep(5)
+    name = ""
+    Health = 100
+    XP = 0
+    start()
+    
 
-    ... make a game
+def unknown():
+        response = ""
+        unknown = randint(1, 6)
+        if unknown == 1:
+            response = "Did I Tell You To Right That?"
+        elif unknown == 2:
+            response = "Do You Realise That You Can't Do That"
+        elif unknown == 3:
+            response = "Error 404. Page Not Found"
+        elif unknown == 4:
+            response = "NO, NO, NO, NO, NO, NO!"
+        else:
+            response = "You Need To Re-think our Life If That Is Your Answer"
+        print(response)
+        start()
+        
+
+def start():
+        print("You have been sent to the land of eternal damnation.")
+        sleep(3)
+        print("However,")
+        sleep(1)
+        print("You are lucky because if you find the lost soul of Argon, you will be freed back to wherever you came from")
+        sleep(3)
+        print("You will be given multiple choice questions and the answers you choose will depend the way the game turns out. So choose wisely")
+
+   
 
 
 
--- quiz.py
+++ quiz.py
@@ -1,8 +1,97 @@
 # Our quiz!
 
 def quiz():
-
-    ... make a quiz!
+    Correct = 0
+    Wrong = 0
+    import time
+    print("Guten Tag")
+    time.sleep(1)
+    Q1 = input("Do you like The Walking Dead? ")
+    
+    if Q1 == "yes":
+          print("Nice You Know What Good Television Is")
+          Correct = Correct + 1
+    else:
+          print("You Disgust Me")
+          Wrong = Wrong + 1
+    time.sleep(1)
+    Q2= input("Has Carl Been Shot In The Eye? ")
+    if Q2 == "yes":
+          print("Well done, you actually have watched The Walkind Dead")
+          Correct = Correct + 1
+    else:
+          print("You haven't watched it. I depise you!")
+          Wrong = Wrong + 1
+    time.sleep(1)
+    Q3 = input("What Is The Tiger In The Walking Dead Called? ")
+    if Q3 == "Shiva":
+          print("Wow, That One Even Baffled Me And You Got It Right")
+          Correct = Correct + 1
+    else:
+          print("No God Damn It!")
+          Wrong = Wrong + 1
+    time.sleep(1)
+    Q4 = input("Is Eugene Dead? ")
+    if Q4 == "no":
+          print("You Are Doing Well")
+          Correct = Correct + 1
+    else:
+          print("ARE YOU SERIOUS!")
+          Wrong = Wrong + 1
+    time.sleep(1)
+    Q5 = input("How Many People Did Neegan Kill? ")
+    if Q5 == "2":
+          print("You Know Your Walking Dead Information")
+          Correct = Correct + 1
+    else:
+          print("That Is Just Completely Incorect")
+          Wrong = Wrong + 1
+    time.sleep(1)
+    Q6 = input("How Many Series Does The Walking Dead Have? ")
+    if Q6 == "7":
+          print("Of Course There Are Currently 7 Seasons")
+          Correct = Correct + 1
+    else:
+          print("7 thats it and yet you still got it wrong")
+          Wrong = Wrong + 1
+    time.sleep(1)
+    Q7 = input("Does Rick Have A Daughter? ")
+    if Q7 == "yes":
+          print("Yep Judith")
+          Correct = Correct + 1
+    else:
+          print("JUDITH!!!!!!!!!! How Do You Not Kow This?")
+          Wrong = Wrong + 1
+    time.sleep(1)
+    Q8 = input("What Is Neegan's Group Called? ")
+    if Q8 == "The Saviours":
+          print("Yes But They Are All Horrible People")
+          Correct = Correct + 1
+    else:
+          print("YOU SERIOUS? It's The Saviours")
+          Wrong = Wrong + 1
+    time.sleep(1)
+    Q9 = input("What Is The Place Where Rick And The Rest Live Called? ")
+    if Q9 == "Alexandria":
+          print("Yes But Unfortunately Neegan And The Saviours Might Overthrow It")
+          Correct = Correct + 1
+    else:
+          print("No, Just No.")
+          Wrong = Wrong + 1            
+    time.sleep(1)
+    
+    Q10 = input("Is There A Pregnant Character? ")
+    if Q10.lower() == "yes":
+          print("Yep but unfortunately the father, SPOILER ALERT, Glen is dead")
+          Correct = Correct + 1
+    else:
+          print("Maggie Is Pregnant Come ON!")
+          Wrong = Wrong + 1                 
+                           
+                     
+                     
+                     
+               
 
 
 
